c686d2f77ee94ddd428dbb781ffd6a3c6c704381
rfc8367 <81213761+rfc8367@users.noreply.github.com>
2021-04-14 20:22:15 +0100

    Update dict.py

--- L5/dict.py
+++ L5/dict.py
@@ -1,3 +1,7 @@
 dict1 = {'first_color': 'Red', 'second_color': 'Green', 'third_color': None}
-dict = {k:v for k, v in dict1.items() if v is not None}
-print(dict)
+
+def dict_fun():
+    dict = {k: v for k, v in dict1.items() if v is not None}
+    print(dict)
+
+dict_fun()
