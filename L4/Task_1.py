c95808c86012ca76f8e0e1efec62a026ad0522f2
rfc8367 <81213761+rfc8367@users.noreply.github.com>
2021-04-15 23:26:31 +0100

    Update Task_1.py

--- L4/Task_1.py
+++ L4/Task_1.py
@@ -1,9 +1,9 @@
-x = int(input("Введите x: "))
-y = int(input("Введите y: "))
+x_var = int(input("Введите x: "))
+y_var = int(input("Введите y: "))
 i = 1
 
-while x < y:
-    x = x + (x * 0.1)
+while x_var < y_var:
+    x_num = x_var + (x_var * 0.1)
     i += 1
 
 print(i)
