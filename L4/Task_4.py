d964ab8499b0ffe202bfa005f986df3855f15bf8
rfc8367 <81213761+rfc8367@users.noreply.github.com>
2021-04-16 00:12:54 +0100

    Update Task_4.py

--- L4/Task_4.py
+++ L4/Task_4.py
@@ -1,6 +1,9 @@
 n = int(input("Введите число: "))
+ladder = ""
 
-for x in range(1, n + 1):
-    for y in range(1, x + 1):
-        print(y, end = '')
-    print()
+if n <= 9:
+    for i in range(1, n + 1):
+        ladder = ladder + str(i)
+        print(ladder)
+else:
+    print("Число больше 9")
