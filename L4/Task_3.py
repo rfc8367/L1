260a596a61a605f066cb9a483ec86455fe24acc7
rfc8367 <81213761+rfc8367@users.noreply.github.com>
2021-04-15 23:25:10 +0100

    Update Task_3.py

--- L4/Task_3.py
+++ L4/Task_3.py
@@ -1,9 +1,9 @@
-a_int = int(input("Введите A: "))
-b_int = int(input("Введите B: "))
+first_int = int(input("Введите A: "))
+second_int = int(input("Введите B: "))
 
-if a_int < b_int:
-    for i in range(a_int, b_int + 1):
+if first_int < second_int:
+    for i in range(first_int, second_int + 1):
         print(i)
 else:
-    for i in range(a_int, b_int - 1, -1):
+    for i in range(first_int, second_int - 1, -1):
         print(i)
