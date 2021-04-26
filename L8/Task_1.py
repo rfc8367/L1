03fcb9ef5d1cdcde2931a9229bedd2d86c723bc0
rfc8367 <81213761+rfc8367@users.noreply.github.com>
2021-04-26 13:15:39 +0100

    Update Task_1.py

--- L8/Task_1.py
+++ L8/Task_1.py
@@ -7,8 +7,16 @@ today = datetime.now()
 result = ['From: ', 'To: ', 'Amount: ', 'Result: ', 'Date: ']
 
 
+def open_file():
+    with open('symbols.json', 'r+') as file:
+        data = json.load(file)
+        symbol_check = data['symbols']
+        if arg.currency_from in symbol_check and arg.currency_to in symbol_check:
+            pars()
 
-def parser():
+
+
+def pars():
 
     if arg.start_date != today:
         start_date = datetime.strptime(arg.start_date, "%Y-%m-%d")
@@ -25,17 +33,6 @@ def parser():
             start_date += timedelta(days=1)
 
 
-
-def open_file():
-    with open('symbols.json', 'r+') as file:
-        data = json.load(file)
-        symbol_check = data['symbols']
-        if arg.currency_from in symbol_check and arg.currency_to in symbol_check:
-            pars()
-
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Online Currency Converter')
     parser.add_argument('currency_from', default = 'USD', type = str)
@@ -44,3 +41,5 @@ if __name__ == '__main__':
     parser.add_argument('--start-date',default = today, type = str)
 
     arg = parser.parse_args()
+
+pars()
