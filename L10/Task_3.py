cac7c6dbba4d3dffd215e40457c5535083580b98
rfc8367 <81213761+rfc8367@users.noreply.github.com>
2021-05-13 08:00:47 +0100

    Update Task_3.py

--- L10/Task_3.py
+++ L10/Task_3.py
@@ -2,7 +2,7 @@ def shift():
     user_list = list(map(int, input("Введите список: ").split()))
     move = int(input("Введите значение: "))
 
-    user_list = user_list[move:] + user_list[:move]
+    user_list = user_list[-move:] + user_list[:-move]
     print(user_list)
 
 shift()
